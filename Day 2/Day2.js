70f3c5fb9081264ad5f7da50bdf5e1b4de276501
Jeysixczs <jc5899787@gmail.com>
2025-06-27 02:45:12 +0800

    Day 1

---
+++ Day 2/Day2.js
@@ -0,0 +1,9 @@
+// innerHTML example
+
+const names = document.getElementById('id1');
+
+alert('successfully changed the name');
+
+names.innerHTML = 'My name is John Carlo';
+names.innerText += ' and I am a web developer';
+
